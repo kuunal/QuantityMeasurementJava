024f5b151153ff4541883d119365b46f346b8f7f
250399 <kunaldeshmukh2503@gmail.com>
2020-05-21 15:37:08 -0700

    ADDED: TEst case for value equality for inch

--- src/main/java/com/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -7,7 +7,8 @@ public class QuantityMeasurement {
 
     double quantity;
 
-    QuantityMeasurement(){}
+    QuantityMeasurement(){
+    }
 
     QuantityMeasurement(double quantity,Units units){
         if (quantity<0)
@@ -20,6 +21,8 @@ public class QuantityMeasurement {
         if(obj == null)
             return false;
         if(this==obj)
+            return true;
+        if(this.getClass()==obj.getClass())
             return true;
         QuantityMeasurement quantityMeasurement = (QuantityMeasurement) obj;
         return Double.compare(this.quantity,quantityMeasurement.quantity)==0;
--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -72,6 +72,14 @@ public class QuantityMeasurementTest {
     }
 
     @Test
+    public void givenInches_WhenValuesAreEqual_ReturnsTrue(){
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(1,INCH);
+        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(1,INCH);
+        Assert.assertTrue(quantityMeasurement1.equals(quantityMeasurement));
+
+    }
+
+    @Test
     public void givenZeroInch_AndZeroFeet_WhenCompared_ReturnsTrue(){
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement(0,FEET);
         QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(0,INCH);
